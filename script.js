77fc562f98e9fdcbce663af8e4a5cf521d090a18
million moss <hurume1999@gmail.com>
2025-04-22 20:06:47 -0400

    Adding the discount property

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ const yogurt = new perishableProduct("Yogurt", 1.5, 10, "2025-10-01");
 const milk = new perishableProduct("Milk", 2.0, 5, "2025-09-15");
 const cheese = new perishableProduct("Cheese", 3.0, 2, "2025-08-20");
 const perishableProducts = [yogurt, milk, cheese];
+
+// Part three//
+class productProperties {
+    static applyDiscount(products, discount) {
+        products.forEach(product => {
+            product.price -= product.price * (discount / 100);
+        });
+    }
+}
